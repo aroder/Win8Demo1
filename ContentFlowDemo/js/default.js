03dc6ad2b773334355d124c9cc23a8f45dd29759
Adam Roderick <aroder@gmail.com>
2011-09-21 19:39:05 -0700

    first commit

---
+++ ContentFlowDemo/js/default.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+    // Uncomment the following line to enable first chance exceptions.
+    // Debug.enableFirstChanceException(true);
+
+    WinJS.Application.onmainwindowactivated = function (e) {
+        if (e.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            // TODO: startup code here
+        }
+    }
+
+    WinJS.Application.start();
+
+
+    $(document).ready(function () {
+        $('#plainOldHtmlButton').click(function () {
+            $('#plainOldHtmlDiv').html('Oh yes, you can!');
+        });
+    });
+
+
+})();
